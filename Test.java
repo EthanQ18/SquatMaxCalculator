4a671363ba9d552df1af8123d5f37513759b2094
Dervonpesnell <60191648+Dervonpesnell@users.noreply.github.com>
2022-04-18 20:36:08 -0400

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+package SquatMaxCalculator;
+
+public abstract class Test { //ABSTRACT CLASS
+	public static void main(String[]args) {
+		SquatMaxCalculator mySquatMaxCalculator= new SquatMaxCalculator(1, "ENERGY");
+		Supplements mySupplements = new  Supplements();
+	}
+
+}
